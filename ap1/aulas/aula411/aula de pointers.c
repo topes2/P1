3b72f2579c4c1634ab26d3a167840191010ad50e
topes2 <l52792@alunos.uevora.pt>
2021-12-03 14:13:38 +0000

    did more stuff like wtf

---
+++ ap1/aulas/aula411/aula de pointers.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int k, k=2;
+//R value ´e o valor da varivavel que neste caso e 2
+//L value ´e o valor de memoria que a varivavel esta guardada indicada por &k
+
+
+// int i = 5;
+//int aux = 7;
+//aux = i; --> aqui ´e o valor de i que neste caso e 5
+//i = aux; --> aqui nao e chamado o valor de i mas sim o seu adereco de memoria
