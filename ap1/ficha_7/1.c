ef72bf149e0d545ee04208b0e3ea7d825c5136b0
topes2 <l52792@alunos.uevora.pt>
2021-12-06 14:14:36 +0000

    fuck you costa

---
+++ ap1/ficha_7/1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int fat1(int n);
+int fat2(int n, int f);
+
+void main(){
+    int n;
+    printf("insira o inicio");
+    scanf("%d",&n);
+    printf("%d",fat1(n));
+    printf("%d",fat2(n,f));
+}
+
+int fat1(int n){
+    int f = 1;
+    while( n > 0){
+        f = n * f;
+        n--;
+    }
+    return f;
+}
+
+int fat2(int n,int f){
+    f = n * f;
+    n--;
+    if (n > 0){
+    fat(n,f);
+    }else if (n == 0){
+        return f;
+    }
+}
+
+//3*2*1
+//3*2*1
--- ap1/ficha_7/2.c
+++ ap1/ficha_7/2.c
@@ -1,31 +1,17 @@
 #include <stdio.h>
-int main() {
 
-  int i, n;
-
-  // initialize first and second terms
-  int t1 = 0, t2 = 1;
-
-  // initialize the next term (3rd term)
-  int nextTerm = t1 + t2;
-
-  // get no. of terms from user
-  printf("Enter the number of terms: ");
-  scanf("%d", &n);
-
-  // print the first two terms t1 and t2
-  printf("Fibonacci Series: %d, %d, ", t1, t2);
-
-  // print 3rd to nth terms
-  for (i = 3; i <= n; ++i) {
-    printf("%d, ", nextTerm);
-    t1 = t2;
-    t2 = nextTerm;
-    nextTerm = t1 + t2;
-  }
-
-  return 0;
+void main(){
+    int n;
+    printf("insira o inicio");
+    scanf("%d",&n);
+    printf("%d",fib(n));
 }
 
-// tenho de pegar no numero e adicionar o anterior
-//tenho de arranjar maneira de quando for 0 ou 1 adicionar 1
+int fib(int n){
+    int f;
+    if(n<2)
+        return 1;
+    else{
+        f=fib(n-1)+ fib(n-2);
+        return f;
+    }
